faade5ee4d0eab242c3c87eb2fe21487d51b4e6d
Damien Guard <damieng@gmail.com>
2016-10-27 17:15:05 -0700

    Suppress info messages when finding a git.exe to use

--- bin/apm.cmd
+++ bin/apm.cmd
@@ -2,10 +2,10 @@
 setlocal enabledelayedexpansion
 
 :: Try to find git.exe in path
-for /f "tokens=*" %%G in ('where git') do set "apm_git_path=%%~dpG"
+for /f "tokens=*" %%G in ('where git 2^>nul') do set "apm_git_path=%%~dpG"
 if not defined apm_git_path (
   :: Try to find git.exe in GitHub Desktop, oldest first so we end with newest
-  for /f "tokens=*" %%d in ('dir /b /s /a:d /od "%LOCALAPPDATA%\GitHub\PortableGit*"') do (
+  for /f "tokens=*" %%d in ('dir /b /s /a:d /od "%LOCALAPPDATA%\GitHub\PortableGit*" 2^>nul') do (
     if exist %%d\cmd\git.exe set apm_git_path=%%d\cmd
   )
   :: Found one, add it to the path
