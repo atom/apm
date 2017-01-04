e8b28ce31dc55890ca978ebe889a3de945d7cd99
Damien Guard <damieng@gmail.com>
2017-01-03 19:09:22 -0800

    Quote all the paths to handle spaces etc. Fixes #666

--- bin/apm.cmd
+++ bin/apm.cmd
@@ -6,15 +6,15 @@ for /f "tokens=*" %%G in ('where git 2^>nul') do set "apm_git_path=%%~dpG"
 if not defined apm_git_path (
   :: Try to find git.exe in GitHub Desktop, oldest first so we end with newest
   for /f "tokens=*" %%d in ('dir /b /s /a:d /od "%LOCALAPPDATA%\GitHub\PortableGit*" 2^>nul') do (
-    if exist %%d\cmd\git.exe set apm_git_path=%%d\cmd
+    if exist "%%d\cmd\git.exe" set "apm_git_path=%%d\cmd"
   )
   :: Found one, add it to the path
   if defined apm_git_path set "Path=!apm_git_path!;!PATH!"
 )
 
-set maybe_node_gyp_path=%~dp0\..\node_modules\node-gyp\bin\node-gyp.js
-if exist %maybe_node_gyp_path% (
-  set npm_config_node_gyp=%maybe_node_gyp_path%
+set "maybe_node_gyp_path=%~dp0\..\node_modules\node-gyp\bin\node-gyp.js"
+if exist "%maybe_node_gyp_path%" (
+  set "npm_config_node_gyp=%maybe_node_gyp_path%"
 )
 
 if exist "%~dp0\node.exe" (
