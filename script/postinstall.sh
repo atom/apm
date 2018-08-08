9a6c185febc800dcc84b54faae722bf3107921ea
Ash Wilson <smashwilson@github.com>
2018-08-08 11:12:11 -0400

    Only rebuild native dependencies if the module version has changed

--- script/postinstall.sh
+++ script/postinstall.sh
@@ -5,9 +5,16 @@ set -e
 echo ">> Downloading bundled Node"
 node script/download-node.js
 
+HOST_MODULE_VERSION=$(node -p 'process.versions.modules')
+BUNDLED_MODULE_VERSION=$(./bin/node -p 'process.versions.modules')
+
 echo
-echo ">> Rebuilding apm dependencies with bundled Node $(./bin/node -p "process.version + ' ' + process.arch")"
-./bin/npm rebuild
+if [ "${HOST_MODULE_VERSION}" != "${BUNDLED_MODULE_VERSION}" ]; then
+  echo ">> Rebuilding apm dependencies with bundled Node $(./bin/node -p "process.version + ' ' + process.arch")"
+  ./bin/npm rebuild
+else
+  echo ">> No need to rebuild dependencies"
+fi
 
 echo
 echo ">> Deduping apm dependencies"
