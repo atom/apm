4a9ebf2a9efd7f87188bc6f09cee5b120e3e5a3d
Ash Wilson <smashwilson@github.com>
2018-09-07 08:23:57 -0400

    Restore npm rebuild call

--- script/postinstall.sh
+++ script/postinstall.sh
@@ -5,16 +5,9 @@ set -e
 echo ">> Downloading bundled Node"
 node script/download-node.js
 
-HOST_MODULE_VERSION=$(node -p 'process.versions.modules')
-BUNDLED_MODULE_VERSION=$(./bin/node -p 'process.versions.modules')
-
 echo
-if [ "${HOST_MODULE_VERSION}" != "${BUNDLED_MODULE_VERSION}" ]; then
-  echo ">> Rebuilding apm dependencies with bundled Node $(./bin/node -p "process.version + ' ' + process.arch")"
-  ./bin/npm rebuild
-else
-  echo ">> No need to rebuild dependencies"
-fi
+echo ">> Rebuilding apm dependencies with bundled Node $(./bin/node -p "process.version + ' ' + process.arch")"
+./bin/npm rebuild
 
 echo
 if [ -z "${NO_APM_DEDUPE}" ]; then
