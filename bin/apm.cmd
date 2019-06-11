66ea37f4ef1c545ac9865d198d2661255a48b822
Winston Liu <50Wliu@users.noreply.github.com>
2019-06-10 22:53:34 -0400

    node-gyp paths in scripts no longer exist

--- bin/apm.cmd
+++ bin/apm.cmd
@@ -12,11 +12,6 @@ if not defined apm_git_path (
   if defined apm_git_path set "Path=!apm_git_path!;!PATH!"
 )
 
-set "maybe_node_gyp_path=%~dp0\..\node_modules\node-gyp\bin\node-gyp.js"
-if exist "%maybe_node_gyp_path%" (
-  set "npm_config_node_gyp=%maybe_node_gyp_path%"
-)
-
 if exist "%~dp0\node.exe" (
   "%~dp0\node.exe" "%~dp0/../lib/cli.js" %*
 ) else (
