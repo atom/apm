7be56b8c544a6922e19809d6587ea6282dbba895
DeeDeeG <DeeDeeG@users.noreply.github.com>
2021-01-13 17:11:54 -0500

    script: Remove automatic deduping from postinstall
    Deduping leaves you with missing packages when installing apm with
    `npm ci`. Modern npm should dedupe automatically to some extent,
    so running dedupe isn't really useful, something like 99% of the time.
    
    If a user wants to ensure apm is installed with maximum deduping,
    they can delete apm's `node_modules` and `package-lock.json` and then
    re-install.
    
    (cherry picked from commit e3218fb440adb0f3a9f05b0739599c9f38dbc8af,
    which was merged previously at the atom-community fork of apm.)

--- script/postinstall.sh
+++ script/postinstall.sh
@@ -8,11 +8,3 @@ node script/download-node.js
 echo
 echo ">> Rebuilding apm dependencies with bundled Node $(./bin/node -p "process.version + ' ' + process.arch")"
 ./bin/npm rebuild
-
-echo
-if [ -z "${NO_APM_DEDUPE}" ]; then
-  echo ">> Deduping apm dependencies"
-  ./bin/npm dedupe
-else
-  echo ">> Deduplication disabled"
-fi
